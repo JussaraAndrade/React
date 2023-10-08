eec4d9597ab87683ff4fa31777dcfe0496b625ea
Jussara Andrade <jussara.jandrade1993@outlook.com>
2023-10-08 20:01:35 -0300

    feat: add login page and form

--- aulas/src/app/pages/login/login.tsx
+++ aulas/src/app/pages/login/login.tsx
@@ -1,20 +1,32 @@
-import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
 
 // Representa página de login
 export const Login = () => {
-    const navigate = useNavigate();
-    // Quando clicar naquele botão quero que faça alguma coisa
-    const handleClick = () => {
-       //Navegue para tela inicial 
-       navigate('/pagina inicial');
+    //tem que iniciar o useState vázio para ser exibido no input
+    const [email, setEmail] = useState('');//useState - Valores dos componentes sejam atualizados
+    const [password, setPassword] = useState('');
+
+    const handleEntrar = () => {
+        console.log(email);
+        console.log(password);
     }
 
     //Tudo que retorna é um componente html
     return(
         <div>
-            Login
-
-            <button onClick={handleClick}>Página Inicial</button>
+            <form>
+                <label>
+                    <span>Email</span>
+                    <input value={email} onChange={e => setEmail(e.target.value)}/>
+                </label>
+                <label>
+                    <span>Senha</span>
+                    <input value={password} onChange={e => setPassword(e.target.value)}/>
+                </label>
+                <button type="button" onClick={handleEntrar}>
+                    Entrar
+                </button>
+            </form>
         </div>
     );
 }
