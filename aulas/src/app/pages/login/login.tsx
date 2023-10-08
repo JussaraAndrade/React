05d6542a2622242903714d1b89b029a853774a5e
Jussara Andrade <jussara.jandrade1993@outlook.com>
2023-10-08 20:29:04 -0300

    feat: example useEffect

--- aulas/src/app/pages/login/login.tsx
+++ aulas/src/app/pages/login/login.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 // Representa página de login
 export const Login = () => {
@@ -6,6 +6,29 @@ export const Login = () => {
     const [email, setEmail] = useState('');//useState - Valores dos componentes sejam atualizados
     const [password, setPassword] = useState('');
 
+
+    //Serve para fazer alguns efeitos nos componentes
+    //Vai ser executar apenas uma vez, somente quando ele é carregado
+    useEffect(() => {
+        //Pergunta somente uma vez!
+        if(window.confirm('Você é homem?')){
+            console.log('Homem')
+        } else {
+            console.log('Mulher')
+        }
+    }, []);
+
+    //Não tem limite uso do useEffect
+    //Consulta de api, ou outra consulta de api, etc...
+    useEffect(() => {
+        //Toda vez que o email ou a senha é alterada o useEffect vai ser executado 
+        console.log(email);
+    }, [email]);
+    
+    useEffect(() => { 
+        console.log(password);
+    }, [password]);
+    
     const handleEntrar = () => {
         console.log(email);
         console.log(password);
@@ -21,7 +44,7 @@ export const Login = () => {
                 </label>
                 <label>
                     <span>Senha</span>
-                    <input value={password} onChange={e => setPassword(e.target.value)}/>
+                    <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
                 </label>
                 <button type="button" onClick={handleEntrar}>
                     Entrar
